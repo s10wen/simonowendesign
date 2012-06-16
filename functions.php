ed2667f80fa8c412a82a4818f2fdb56f28998607
Simon Owen Design <simon.owen.design@gmail.com>
2012-06-16 11:51:00 +0100

    Updated gravatar width and styles. Removed timestamp from comments and moved date from footer. Added box-shadow mixin. Added initial styles for comments. Removed css comment regarding fonts. Added light grey colour variable.

--- functions.php
+++ functions.php
@@ -320,19 +320,17 @@ function boilerplate_comment( $comment, $args, $depth ) {
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
 		<article id="comment-<?php comment_ID(); ?>">
 			<div class="comment-author vcard">
-				<?php echo get_avatar( $comment, 40 ); ?>
-				<?php printf( __( '%s <span class="says">says:</span>', 'boilerplate' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+				<?php echo get_avatar( $comment, 60 ); ?>
+				<?php printf( __( '%s', 'boilerplate' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+				<?php
+					/* translators: 1: date, 2: time */
+					printf( __( '<div class="date">%1$s</div>', 'boilerplate' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'boilerplate' ), ' ' );
+				?>
 			</div><!-- .comment-author .vcard -->
 			<?php if ( $comment->comment_approved == '0' ) : ?>
 				<em><?php _e( 'Your comment is awaiting moderation.', 'boilerplate' ); ?></em>
 				<br />
 			<?php endif; ?>
-			<footer class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
-				<?php
-					/* translators: 1: date, 2: time */
-					printf( __( '%1$s at %2$s', 'boilerplate' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'boilerplate' ), ' ' );
-				?>
-			</footer><!-- .comment-meta .commentmetadata -->
 			<div class="comment-body"><?php comment_text(); ?></div>
 			<div class="reply">
 				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
