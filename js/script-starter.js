b1e6f4436e225b10317eab9f0e53f78627a0bfcf
Simon Owen Design <simon.owen.design@gmail.com>
2012-11-11 15:55:28 +0000

    Revert "Added full footer link names in for mobile menu."
    This reverts commit 23a3ddd9d7cfbe98c0f522bbf304685c3e3978c9.

--- js/script-starter.js
+++ js/script-starter.js
@@ -1,5 +1,9 @@
-// To The Top
+//plugin
 jQuery.fn.topLink = function(settings) {
+	settings = jQuery.extend({
+		min: 1,
+		fadeSpeed: 200
+	}, settings);
 	return this.each(function() {
 		//listen for scroll
 		var el = $(this);
@@ -17,94 +21,16 @@ jQuery.fn.topLink = function(settings) {
 	});
 };
 
+//usage w/ smoothscroll
 $(document).ready(function() {
-
-
-	// To The Top - Custom
-	var scroll = '.scroll';
-
 	//set the link
-	$(scroll).topLink({
+	$('.scroll').topLink({
 		min: 400,
 		fadeSpeed: 500
 	});
 	//smoothscroll
-	$(scroll).click(function(e) {
+	$('.scroll').click(function(e) {
 		e.preventDefault();
 		$.scrollTo(0,300);
 	});
-
-
-	// Facebook style menu
-	var pagebody = $("body");
-	var topbar   = $("#toolbarnav");
-	var content  = $("section");
-	var viewport = {
-		width  : $(window).width(),
-		height : $(window).height()
-	};
-	// retrieve variables as
-	// viewport.width / viewport.height
-	
-	function openme() {
-		$(function () {
-			topbar.animate({
-				left: "290px"
-			}, { duration: 300, queue: false });
-			pagebody.animate({
-				left: "290px"
-			}, { duration: 300, queue: false });
-		});
-	}
-	
-	function closeme() {
-		$(function() {
-			topbar.animate({
-				left: "0px"
-			}, { duration: 180, queue: false });
-			pagebody.animate({
-				left: "0px"
-			}, { duration: 180, queue: false });
-		});
-	}
-
-	// checking whether to open or close nav menu
-	$("#menu-btn").live("click", function(e){
-		e.preventDefault();
-		var leftval = pagebody.css('left');
-		
-		if(leftval == "0px") {
-			openme();
-		}
-		else {
-			closeme();
-		}
-	});
-	
-	// loading page content for navigation
-	$("a.navlink").live("click", function(e){
-		e.preventDefault();
-		var linkurl     = $(this).attr("href");
-		var linkhtmlurl = linkurl.substring(1, linkurl.length);
-		
-		var imgloader   = '<center style="margin-top: 30px;"><img src="img/preloader.gif" alt="loading..." /></center>';
-		
-		closeme();
-		
-		$(function() {
-			topbar.css("top", "0px");
-			window.scrollTo(0, 1);
-		});
-		
-		content.html(imgloader);
-		
-		setTimeout(function() {
-			content.load(linkhtmlurl, function() { /* no callback */ });
-		}, 1200);
-	});
 });
-
-// Move footer to navigation
-if (document.documentElement.clientWidth < 768) {
-	$("footer").appendTo("nav");
-}
