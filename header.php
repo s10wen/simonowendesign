5f87ab4c11512e339d332a8819cd2bfa8e27fc9e
Simon Owen Design <simon.owen.design@gmail.com>
2012-11-19 00:23:28 +0000

    Updated Google App Engine File to correctly serve .css file. Updated css link to pull style.css from Google App Engine location.

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
-		<link rel="stylesheet" href="<?php bloginfo( 'template_url'); ?>/styles/css/style.css" />
+		<link rel="stylesheet" href="http://simonowendesign.appspot.com/style.css" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 		<?php wp_head(); ?>
