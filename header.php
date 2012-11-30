509a3d18321f45643de7885a39f4d862ff218ef6
Simon Owen Design <simon.owen.design@gmail.com>
2012-11-30 21:33:42 +0000

    Updated Google Analytics.

--- header.php
+++ header.php
@@ -59,15 +59,17 @@
 		<link rel="apple-touch-startup-image" sizes="768x1004" href="http://simonowendesign.appspot.com/img/startup-768x1004.png" media="screen and (min-device-width : 481px) and (max-device-width : 1024px) and (orientation : portrait)">
 
 		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-28329112-1']);
-		  _gaq.push(['_trackPageview']);
+			var _gaq = _gaq || [];
+				_gaq.push(['_setAccount', 'UA-28329112-1']);
+				_gaq.push(['_setDomainName', 'simonowendesign.co.uk']);
+				_gaq.push(['_setAllowLinker', true]);
+				_gaq.push(['_trackPageview']);
 
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 		</script>
 
 	</head>
